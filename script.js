a5c78bc90110e92efe335bc94b0c32a7a2b321e2
Alexander Ela <ela.alexander@gmail.com>
2018-07-14 12:37:26 -0600

    Change js variables to Ashton classes

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //Variables: header
-var titleInput = document.querySelector(".alex-title-input");
+var titleInput = document.querySelector(".form-inputs");
 var bodyInput = document.querySelector(".alex-body-input");
-var saveBtn = document.querySelector(".alex-save-button");
-var searchInput = document.querySelector(".alex-search-box");
-
+var saveBtn = document.querySelector(".save-idea-btn");
 //Variables: ideas
 var titleOutput = document.querySelector(".alex-title-output");
-var bodyOutput = document.querySelector(".alex-body-output");
-var qualityRate = document.querySelector(".alex-quality-rating");
-var ideaSection = document.querySelector(".alex-idea-entry");
+var bodyOutput = document.querySelector(".idea-body");
+var qualityRate = document.querySelector(".quality-value");
+var ideaSection = document.querySelector(".idea-list");
 var newSection = document.createElement("article");
 
 
@@ -24,10 +22,10 @@ function saveIdea(event) {
 	var newEntry = `<article aria-label="idea entry">
 						<h2 class="alex-title-output">${titleInput.value}</h2>
 						<img src="Icons/delete.svg" alt="delete icon">
-						<p class="alex-body-output">${bodyInput.value}</p>
+						<p class="idea-body">${bodyInput.value}</p>
 						<img src="Icons/upvote.svg" alt="quality up-vote icon">
 						<img src="Icons/downvote.svg" alt="quality down-vote icon">
-						<p>quality: </p><span class="alex-quality-rating">swill</span>
+						<p>quality: </p><span class="quality-value">swill</span>
 						<hr class="section-break"></article>`;
 	newSection.innerHTML = newEntry;
 	ideaSection.prepend(newSection);
