ba310b995eb933de1f649303c79741282a590e6e
Alexander Ela <ela.alexander@gmail.com>
2018-07-14 14:21:37 -0600

    Combine html, css, prelim. javascript with correct variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Variables: header
-var titleInput = document.querySelector(".form-inputs");
-var bodyInput = document.querySelector(".alex-body-input");
+var titleInput = document.querySelector("#idea-title-input");
+var bodyInput = document.querySelector("#idea-body-input");
 var saveBtn = document.querySelector(".save-idea-btn");
 //Variables: ideas
-var titleOutput = document.querySelector(".alex-title-output");
+var titleOutput = document.querySelector(".idea-title-output");
 var bodyOutput = document.querySelector(".idea-body");
 var qualityRate = document.querySelector(".quality-value");
 var ideaSection = document.querySelector(".idea-list");
@@ -20,12 +20,14 @@ saveBtn.addEventListener("click", saveIdea);
 function saveIdea(event) {
 	event.preventDefault();
 	var newEntry = `<article aria-label="idea entry">
-						<h2 class="alex-title-output">${titleInput.value}</h2>
-						<img src="Icons/delete.svg" alt="delete icon">
+						<div class="list-title">
+						<h2 class="idea-title-output">${titleInput.value}</h2>
+						<img src="Icons/delete.svg" alt="delete icon" class="vote-delete-btns"></div>
 						<p class="idea-body">${bodyInput.value}</p>
-						<img src="Icons/upvote.svg" alt="quality up-vote icon">
-						<img src="Icons/downvote.svg" alt="quality down-vote icon">
-						<p>quality: </p><span class="quality-value">swill</span>
+						<div class="voting-content">
+						<img src="Icons/upvote.svg" alt="quality up-vote icon" class="vote-delete-btns">
+						<img src="Icons/downvote.svg" alt="quality down-vote icon" class="vote-delete-btns">
+						<p>quality: </p><span class="quality-value">swill</span></div>
 						<hr class="section-break"></article>`;
 	newSection.innerHTML = newEntry;
 	ideaSection.prepend(newSection);
