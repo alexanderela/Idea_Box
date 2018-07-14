ac4d244f7691f346a0a21497ed0651f5cc1d4f4d
Alexander Ela <ela.alexander@gmail.com>
2018-07-14 17:21:01 -0600

    Brainstorm JS constructor function ideas proposed by Kent

--- script.js
+++ script.js
@@ -12,13 +12,27 @@ var newSection = document.createElement("article");
 
 
 
+
 // Event Listeners
 saveBtn.addEventListener("click", saveIdea);
 
 
 //Functions
-function saveIdea(event) {
+
+
+
+
+
+function ideaCard(saveIdea) {
+  this.id = something
+  this.title = titleInput.value
+  this.body = bodyInput.value
+  this.quality = something
+};
+
+ideaCard.prototype.saveIdea = function() {
 	event.preventDefault();
+	console.log("it's lit")
 	var newEntry = `<article aria-label="idea entry">
 						<div class="list-title">
 						<h2 class="idea-title-output">${titleInput.value}</h2>
@@ -31,7 +45,58 @@ function saveIdea(event) {
 						<hr class="section-break"></article>`;
 	newSection.innerHTML = newEntry;
 	ideaSection.prepend(newSection);
+	document.getElementById("idea-form").reset();
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`<article aria-label="idea entry">
+<div class="list-title">
+<h2 class="idea-title-output">${titleInput.value}</h2>
+<img src="Icons/delete.svg" alt="delete icon" class="vote-delete-btns"></div>
+<p class="idea-body">${bodyInput.value}</p>
+						<div class="voting-content">
+						<img src="Icons/upvote.svg" alt="quality up-vote icon" class="vote-delete-btns">
+						<img src="Icons/downvote.svg" alt="quality down-vote icon" class="vote-delete-btns">
+						<p>quality: </p><span class="quality-value">swill</span></div>
+						<hr class="section-break"></article>`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
